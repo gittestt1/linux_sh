921a204fd35e399eb1c069802218e56e59018085
gittestt1 <101639602+gittestt1@users.noreply.github.com>
2023-06-10 00:04:24 +0800

    Create vast_init.sh

---
+++ vast_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update -y &&apt upgrade -y &&
+apt install sudo wget curl axel zip fuse -y &&
+curl https://rclone.org/install.sh | bash &&
+wget https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py -O /bin/systemctl &&
+sudo chmod a+x /bin/systemctl &&
+bash <(curl -Ls https://raw.githubusercontent.com/gittestt1/ffmpeg-instrall/main/install.sh) &&
+restart
